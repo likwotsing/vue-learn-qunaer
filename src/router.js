1338035fc9546e9469880bf03509f0a4a324e3c7
likwotsing <likwotsing@gmail.com>
2019-03-25 21:30:00 +0800

    add 6-3

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '@/pages/home/Home.vue'
+import List from '@/pages/list/List.vue'
 
 Vue.use(Router)
 
@@ -8,16 +9,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/list',
+      name: 'List',
+      component: List
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
