025d5b842762f39a883ca8050a2a028622756dbe
likwotsing <likwotsing@gmail.com>
2019-03-26 21:37:22 +0800

    add 7-2, iconfont的使用和配置别名

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import fastClick from 'fastclick'
 import App from './App.vue'
 import router from './router'
-import '@/assets/styles/reset.css'
-import '@/assets/styles/border.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+
+// styles在vue.config.js里配置了别名
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市<span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -13,24 +20,34 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  // 引入css，使用@import，使用别名时前面要添加~
   .header
     display: flex
     line-height: .86rem
-    background: #00bcd4
+    background: $bgColor
     color: #fff
     .header-left
       float: left
       width: .64rem
+      .back-icon
+        text-align: center
     .header-input
       flex: 1
       height: .64rem
+      line-height: .64rem
       margin-top: .12rem
       margin-left: .2rem
+      padding-left: .2rem
       background: #ffffff
+      color: #ccc
       border-radius: .1rem
     .header-right
       float: right
       width: 1.2rem
       text-align: center
+      .arrow-icon
+        margin-left: .04rem
+        font-size: .24rem
 
 </style>
