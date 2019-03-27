d9f19baf43b2b3960f1c5283f0528d51549a74a0
likwotsing <likwotsing@gmail.com>
2019-03-27 22:34:32 +0800

    add 7.4, weekend组件

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -78,26 +78,28 @@ export default {
 .icons >>> .swiper-container
     height: 0
     padding-bottom: 50%
-.icon
-  position relative
-  float: left
-  overflow: hidden
-  width: 25%
-  height: 0
-  padding-bottom: 25%
-  .icon-img
-    position: absolute
-    top: 0
-    right: 0
-    bottom: .44rem
-    left: 0
-    .icon-img-content
-      display: block
-      height: 100%
-      margin: 0 auto
-    .icon-desc
-      height: .44rem
-      line-height: .44rem
-      text-align: center
-      ellipsis()
+.icons
+  margin-top: .2rem
+  .icon
+    position relative
+    float: left
+    overflow: hidden
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      right: 0
+      bottom: .44rem
+      left: 0
+      .icon-img-content
+        display: block
+        height: 100%
+        margin: 0 auto
+      .icon-desc
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        ellipsis()
 </style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom"
+        v-for="item of recommendList"
+        :key="item.id"
+      >
+        <img class="item-img" :src="item.imgUrl" alt="">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// 组件的一部分内容，希望父组件去定制的时候，使用slot
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9e/21df651e19af5d.jpg_r_640x214_3ea5bb38.jpg',
+        title: '大连圣亚海洋世界大连圣亚海洋世界大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫都海洋主题乐园浪漫大连首站，浪漫都海洋主题乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9e/21df651e19af5d.jpg_r_640x214_3ea5bb38.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫都海洋主题乐园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9e/21df651e19af5d.jpg_r_640x214_3ea5bb38.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫都海洋主题乐园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    .item-img
+      width: 100%
+      height: 33.9%
+    .item-info
+      flex: 1
+      padding: .1rem
+      overflow: hidden
+      // 以下2种也可以实现
+      // width: 0
+      // min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-btn
+        line-height: .44rem
+        margin-top: .16rem
+        padding: 0 .2rem
+        background: #ff9300
+        border-radius: .06rem
+</style>
