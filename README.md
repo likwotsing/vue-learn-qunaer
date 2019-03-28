586342b6e11460a2e8cc10fa91030c59650c2ea8
likwotsing <likwotsing@gmail.com>
2019-03-28 10:03:05 +0800

    add 7-8, index-ajax分支，mock数据

--- README.md
+++ README.md
@@ -27,3 +27,8 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 静态文件
+1. vue-cli3没有static文件夹，默认的静态文件夹是public
+2. 例如，在public里新建一个mock文件夹，里面有一个index.json文件，浏览器访问：http://localhost:8080/mock/index.json
+3. 配置代理proxy，参考vue.config.js
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,32 @@
-const path = require("path");
-function resolve(dir) {
-  return path.join(__dirname, dir);
+const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, dir)
 }
+// const goods = require('./static/goods.json')
+
 module.exports = {
   lintOnSave: true,
   chainWebpack: config => {
     config.resolve.alias
-      .set("@", resolve("src"))
-      .set("styles", resolve("src/assets/styles"));
+      .set('@', resolve('src'))
+      .set('styles', resolve('src/assets/styles'))
+  },
+  publicPath: './',
+  assetsDir: 'static',
+  // webpack-dev-server
+  devServer: {
+    // before: function (app, server) {
+    //   app.get('/goods', function (req, res) {
+    //     res.json(goods)
+    //   })
+    // },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        pathRewrite: {
+          '^/api': '/mock'
+        }
+      }
+    }
   }
-};
+}
