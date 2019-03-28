3d89bce4b91661dc10368d6bda81bd49e713d2b3
likwotsing <likwotsing@gmail.com>
2019-03-28 20:03:37 +0800

    add 8-7, 列表切换性能优化

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -23,7 +23,9 @@ export default {
   },
   data () {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
   },
   computed: {
@@ -35,6 +37,10 @@ export default {
       return letters
     }
   },
+  update () {
+    // 优化1：starY是固定的，组件从ajax请求数据完成后重新渲染再更新一次即可
+    this.startY = this.$refs['A'][0].offsetTop
+  },
   methods: {
     handleLetterClick (e) {
       this.$emit('change', e.target.innerText)
@@ -44,16 +50,23 @@ export default {
     },
     handleTouchMove (e) {
       if (this.touchStatus) {
-        const startY = this.$refs['A'][0].offsetTop
-        // 79，是.list元素绝对定位的top值，即header和search的高度
-        const touchY = e.touches[0].clientY - 79
-        // 20，是每个字母的高度
-        const index = Math.floor((touchY - startY) / 20)
-        // console.log(this.letters[index])
-        // 用if判断是否超出范围
-        if (index >= 0 && index < this.letters.length) {
-          this.$emit('change', this.letters[index])
+        // starY是一个固定的，只在update里计算一次即可
+        // const startY = this.$refs['A'][0].offsetTop
+
+        // 优化2：判断timer是否存在，若存在就清除。16毫秒后再执行(节流，最小间隔是16毫秒)
+        if (this.timer) {
+          clearTimeout(this.timer)
         }
+        this.timer = setTimeout(() => {
+          // 79，是.list元素绝对定位的top值，即header和search的高度
+          // 20，是每个字母的高度
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 20)
+          // 用if判断是否超出范围
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
       }
     },
     handleTouchEnd () {
