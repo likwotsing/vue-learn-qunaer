62314d0b7444e3ce2eb2b38a62f32527c6131ac8
likwotsing <likwotsing@gmail.com>
2019-03-29 12:10:39 +0800

    add 9-2, detail-banner, 公用图片画廊组件拆分

---
+++ src/common/gallery/Gallery.vue
@@ -0,0 +1,76 @@
+<template>
+  <div
+    class="container"
+    @click="handleGalleryClick"
+  >
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <swiper-slide
+          v-for="(item, index) of imgs"
+          :key="index"
+        >
+          <img class="gallery-img" :src="item" />
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallery',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  methods: {
+    handleGalleryClick () {
+      this.$emit('close')
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        // swiper显示/隐藏或者修改swiper子元素时，自动初始化swiper
+        observer: true,
+        // 父元素变化时，swiper更新
+        observeParents: true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container >>> .swiper-container
+    overflow: inherit
+  .container
+    display: flex
+    justify-content: center
+    align-items: center
+    z-index: 99
+    position: fixed
+    top: 0
+    right: 0
+    bottom: 0
+    left: 0
+    background: #000
+    .wrapper
+      // overflow: hidden
+      width: 100%
+      height: 0
+      padding-bottom: 66.4%
+      background: #fff
+      .gallery-img
+        width: 100%
+      .swiper-pagination
+        bottom: -1rem
+        color: #fff
+</style>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,21 +1,45 @@
 <template>
-  <div class="banner">
-    <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1508/2c/e8136f95b4ce7866337c72730167b1a3.water.jpg_r_800x800_d8ac3143.jpg" alt="">
-    <div class="banner-info">
-      <div class="banner-title">
-        鸟巢(AAAAA景区)
-      </div>
-      <div class="banner-number">
-        <span class="iconfont banner-icon">&#xe663;</span>
-        <span> 6</span>
+  <div>
+    <div
+      class="banner"
+      @click="handleBannerClick"
+    >
+      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1508/2c/e8136f95b4ce7866337c72730167b1a3.water.jpg_r_800x800_d8ac3143.jpg" alt="">
+      <div class="banner-info">
+        <div class="banner-title">
+          鸟巢(AAAAA景区)
+        </div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe663;</span>
+          <span> 6</span>
+        </div>
       </div>
     </div>
+    <common-gallery :imgs="imgs" v-show="showGallery" @close="handleGalleryClose"></common-gallery>
   </div>
 </template>
 
 <script>
+import CommonGallery from 'common/gallery/Gallery'
 export default {
-  name: 'DetailBanner'
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallery: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1410/e3/73da8d3e19cdc41c1932d4fcd22ec792.water.jpg_r_800x800_7f96ccf9.jpg', 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_r_800x800_6edd8174.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallery = true
+    },
+    handleGalleryClose () {
+      this.showGallery = false
+    }
+  },
+  components: {
+    CommonGallery
+  }
 }
 </script>
 
